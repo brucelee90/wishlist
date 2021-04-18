0b72428ac51195d922810db8b677c15c52b29511
Lee <leeklopfers@gmx.de>
2021-04-18 23:58:37 +0200

    15 - Pure JavaScript Wishlist Toggle For Shopify App

--- resources/js/l4-wishlist.js
+++ resources/js/l4-wishlist.js
@@ -1,3 +1,27 @@
 import '../css/custom.css'
 
-console.log('TESTING')
+let wishlistBtn = document.querySelector('.l4-wishlist')
+let activeClass = 'l4-active'
+
+const addToWishlist = (elem) => {
+    console.log('Add to wishlist')
+    elem.classList.add(activeClass)
+    elem.innerText = 'Remove from Wishlist'
+}
+
+const removeFromWishlist = (elem) => {
+    console.log('Remove from to wishlist')
+    elem.classList.remove(activeClass)
+    elem.innerText = 'Add To Wishlist'
+
+}
+
+
+wishlistBtn.addEventListener('click', function(){
+    if (!this.classList.contains(activeClass)){
+        addToWishlist(this)
+    }else {
+        removeFromWishlist(this)
+    }
+
+})
